1ad8b049c68563d902aeefa92c8a59fffce0a9ab
Beatriz Andrade <beatrizandrade020409@gmail.com>
2026-06-21 23:49:41 -0300

    Ajustando arquivos

--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -33,7 +33,7 @@ const PlaceOrderScreen = () => {
       navigate(`/order/${order._id}`);
       dispatch({ type: ORDER_CREATE_RESET });
     }
-  }, [success, navigate, dispatch]);
+  }, [success, navigate, dispatch ,order]);
 
   const placeOrderHandler = () => {
     dispatch(
